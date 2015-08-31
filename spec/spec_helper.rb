225d22f0eb261a68d8ff7a07ec3f6eb70463b5be
Sebastián Katzer <katzer.sebastian@googlemail.com>
2015-08-31 10:23:52 +0200

    More test coverage setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,5 +11,13 @@ RSpec::Matchers.define :respond_to_public do |expected|
   match { |actual| actual.public_methods.include? expected }
 end
 
+WebMock.disable_net_connect!(allow: 'codeclimate.com')
+
 CodeClimate::TestReporter.start
-SimpleCov.start
+
+SimpleCov.start do
+  formatter SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    CodeClimate::TestReporter::Formatter
+  ]
+end
