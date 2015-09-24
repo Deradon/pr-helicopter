d0132732a20bd821a236c2e1b3e1cadfc7576a8f
Sebastián Katzer <katzer.sebastian@googlemail.com>
2015-09-24 12:53:57 +0200

    Filter out spec files and eager load source files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,8 +16,11 @@ WebMock.disable_net_connect!(allow: 'codeclimate.com')
 CodeClimate::TestReporter.start
 
 SimpleCov.start do
+  add_filter '/spec'
   formatter SimpleCov::Formatter::MultiFormatter[
     SimpleCov::Formatter::HTMLFormatter,
     CodeClimate::TestReporter::Formatter
   ]
 end
+
+Dir['lib/**/*.rb'].each { |f| require f.sub('lib/', '') }
