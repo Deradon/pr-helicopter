20e5a757aad9cd5d68af532d18393fbb4e0d6910
Sebastián Katzer <katzer.sebastian@googlemail.com>
2015-09-29 13:57:41 +0200

    Ensure that attire readers are presents

--- spec/shared/base_notification.rb
+++ spec/shared/base_notification.rb
@@ -1,5 +1,6 @@
 
 RSpec.shared_examples 'a Helicopter::Notification::Base' do
   subject { described_class.new from: '', subject: '', body: '' }
+  it { is_expected.to respond_to(:from, :subject, :body, :credentials) }
   it { is_expected.to respond_to(:send_to).with(1).argument }
 end
